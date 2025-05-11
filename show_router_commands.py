c5ba24be4294e44270b650a8095895d118711f38
shafiq-mir <itechno32@gmail.com>
2025-05-11 18:03:10 -0400

    changing ip to 10.0.0.81

--- show_router_commands.py
+++ show_router_commands.py
@@ -11,7 +11,7 @@ from netmiko import ConnectHandler
 
 connection_to_router = ConnectHandler(
                       device_type="cisco_ios",
-                      host="10.0.0.80",
+                      host="10.0.0.81",
                       username="shafiq",
                       password="cisco",
 )
